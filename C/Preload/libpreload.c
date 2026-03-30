b7e5775088660d2f48ee78796a07627846f91fd5
Ruslan Zalata <rz@fabmicro.ru>
2026-03-30 05:23:08 +0500

    C/Preload: Add calling to old malloc, get its address by calling to dlsym()

--- C/Preload/libpreload.c
+++ C/Preload/libpreload.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <dlfcn.h>
+
+void *(*old_malloc)(size_t size) = NULL;
 
 void* malloc(long unsigned int size) {
 	char str[512];
+
+	if(old_malloc == NULL) {
+		old_malloc = dlsym(RTLD_NEXT, "malloc");
+		int str_size = snprintf(str, 512, "MALLOC: obtained old_malloc = %p\r\n", old_malloc);
+		write(1, str, str_size);
+		if(old_malloc == NULL)
+			return 0;
+	}
+
+
 	int str_size = snprintf(str, 512, "MALLOC: %lu\r\n", size);
 	write(1, str, str_size);
-	return 0;
+	return old_malloc(size);
 }
--- C/Preload/libpreload.cpp
+++ C/Preload/libpreload.cpp
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <iostream>
+#include <dlfcn.h>
 
 extern "C" {
 
-void* malloc(long unsigned int size) {
+void *(*old_malloc)(size_t size) = NULL;
+
+void* malloc(size_t size) {
 	char str[512];
+
+	if(old_malloc == NULL) {
+		old_malloc = (void *(*)(size_t))dlsym(RTLD_NEXT, "malloc");
+		int str_size = snprintf(str, 512, "MALLOC: obtained old_malloc = %p\r\n", old_malloc);
+		write(1, str, str_size);
+		if(old_malloc == NULL)
+			return 0;
+	}
+
 	int str_size = snprintf(str, 512, "MALLOC: %lu\r\n", size);
 	write(1, str, str_size);
-	return 0;
+	return old_malloc(size);
 }
 
 }
