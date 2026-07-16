f0a85801210f2cb63da46f97d8bf450a686a6b84
Ruslan Zalata <rz@fabmicro.ru>
2026-07-17 01:46:05 +0500

    Extend C/printf_hack

--- C/printf_hack/printf_hack.c
+++ C/printf_hack/printf_hack.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-	char *a = "World";
+	char *a = "Hello";
 	int n = 0;
+	char x = 0;
+	char y = 0;
 	printf("n = %d\n", n);
-	printf("Hello %s%n\n", a, &n);
+	printf("%s%n\n", a, &n); // initial set n = sizeof("Hello")
 	printf("n = %d\n", n);
+	printf("%100d%n\n", 123, &n); // add 100 to n
+	printf("n = %d\n", n);
+	printf("%.33d%n\n", 123, &n); // set n = 33
+	printf("n = %d\n", n);
+	printf("%s%s%s%4$hhn\n", a, a, a, &x); // store lower byte of accumulator to arg4
+	printf("x = %d\n", x); 
+	printf("%1$255d%hhn\n", 123, &y); // dec accumulator by 1 
+	printf("y = %d\n", y);
 	return 0;
 }
